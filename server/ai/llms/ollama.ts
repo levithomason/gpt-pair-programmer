5a746d73b6680d4ff6207c04c5dfdb9b2f23199c
Levi Thomason <me@levithomason.com>
2023-10-10 16:34:43 -0700

    add ollama models

--- server/ai/llms/ollama.ts
+++ server/ai/llms/ollama.ts
@@ -1,14 +1,28 @@
 import { Ollama } from "langchain/llms/ollama";
 
-const model = [
-  //
-  "mistral",
-  "codellama:7b",
-  "llama2",
-][0];
+// TODO: These rely on Ollama being installed and running locally.
+//       There should be some validation to ensure that's the case.
 
-export const ollamaLLM = new Ollama({
+export const mistralLLM = new Ollama({
   baseUrl: "http://localhost:11434",
-  model,
-  temperature: 0.5,
+  model: "mistral",
+  temperature: 1,
+});
+
+export const mistralInstructLLM = new Ollama({
+  baseUrl: "http://localhost:11434",
+  model: "mistral:7b-instruct",
+  temperature: 1,
+});
+
+export const codellamaLLM = new Ollama({
+  baseUrl: "http://localhost:11434",
+  model: "codellama",
+  temperature: 1,
+});
+
+export const llama2LLM = new Ollama({
+  baseUrl: "http://localhost:11434",
+  model: "llama2",
+  temperature: 1,
 });
