d7d37b7004f00774898d0f33e5c85552c2acab44
Levi Thomason <me@levithomason.com>
2023-10-12 19:39:05 -0700

    remove trimStringToTokens

--- server/utils/tokens.ts
+++ server/utils/tokens.ts
@@ -7,27 +7,6 @@ import { OPENAI_MODELS } from "../../shared/config.js";
 
 const log = debug("gpp:server:utils:tokens");
 
-/**
- * Trims a string to a certain number of estimated tokens.
- * If the string is less than the number of tokens, the string is returned as-is.
- * If truncation is required, an "..." will be inserted in the middle of the string.
- * @param {string} str
- * @param {number} tokens
- */
-export const trimStringToTokens = (str: string, tokens: number) => {
-  const maxCharacters = tokens * 4;
-
-  if (str.length > maxCharacters) {
-    const divider = "\n\n[TRUNCATED]\n\n";
-    const start = str.slice(0, maxCharacters / 2 - divider.length);
-    const end = str.slice(-maxCharacters / 2 + divider.length);
-
-    return start + divider + end;
-  }
-
-  return str;
-};
-
 //
 // Token Counter
 // See: https://github.com/dqbd/tiktoken/blob/main/js/examples/dynamic.ts
