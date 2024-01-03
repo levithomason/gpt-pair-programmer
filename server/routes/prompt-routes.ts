1cebbd6a0f480ad0bd0234a8649eb24ffd2cf414
Levi Thomason <me@levithomason.com>
2024-01-02 17:14:49 -0800

    Add /prompts/chatgpt endpoint

--- server/routes/prompt-routes.ts
+++ server/routes/prompt-routes.ts
@@ -4,17 +4,29 @@ import debug from "debug";
 import { promptSystemDefault } from "../ai/prompts.js";
 import { settings } from "../settings.js";
 import { getLLM } from "../ai/llms/index.js";
+import { chatGPTFunctionsPrompt } from "../utils/index.js";
 
 const log = debug("gpp:server:routes:settings");
 
 export const promptRoutes = express.Router();
 
-promptRoutes.get("/prompts/system", async (_, res) => {
-  const llm = getLLM(settings.modelName);
-  const prompt = await promptSystemDefault();
+promptRoutes
+  .get("/prompts/system", async (_, res) => {
+    const llm = getLLM(settings.modelName);
+    const prompt = await promptSystemDefault();
 
-  res.send({
-    prompt,
-    tokens: await llm.countTokens(prompt),
+    res.send({
+      prompt,
+      tokens: await llm.countTokens(prompt),
+    });
+  })
+
+  .get("/prompts/chatgpt", async (_, res) => {
+    const llm = getLLM(settings.modelName);
+    const prompt = chatGPTFunctionsPrompt;
+
+    res.send({
+      prompt,
+      tokens: await llm.countTokens(prompt),
+    });
   });
-});
